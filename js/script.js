1b8bf7954ffb6a618583aa59f0c95cf37357512d
Rachmawati09 <chyntiacitra04@gmail.com>
2020-12-21 09:36:17 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const txtElement = ['seorang Mahasiswa'];
+let count = 0;
+let txtindex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+    if(count == txtElement.length){
+        count = 0;
+    }
+
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtindex);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if(words.length == currentTxt.length){
+        count++;
+        txtindex = 0;
+    }
+
+    setTimeout(ngetik, 500);
+})();
